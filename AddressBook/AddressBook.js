c7dc5ecf552f8776989000805c51577b274b5d7d
abhi5926 <abhisheksen5926@gmail.com>
2025-03-04 07:40:59 +0530

    [Abhishek] Add. JS AddressBook Adding Ability to ensure Valid Contacts are added with the help of Regex

--- AddressBook/AddressBook.js
+++ AddressBook/AddressBook.js
@@ -1,5 +1,27 @@
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phone, email) {
+
+        let nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+        let addressRegex = /^[A-Za-z0-9\s]{4,}$/;
+        let zipRegex = /^\d{6}$/;
+        let phoneRegex = /^[7-9]\d{9}$/;
+        let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+            throw new Error("Invalid name, should have at least 3 characters.");
+        }
+        if (!addressRegex.test(address) || !addressRegex.test(city) || !addressRegex.test(state)) {
+            throw new Error("Invalid Address, City or State, Should have at least 4 characters.");
+        }
+        if (!zipRegex.test(zip)) {
+            throw new Error("Invalid Zip, Should be exactly 6 digits.");
+        }
+        if (!phoneRegex.test(phone)) {
+            throw new Error("Invalid Phone Number, Should be a 10-digit number starting with 7, 8, or 9.");
+        }
+        if (!emailRegex.test(email)) {
+            throw new Error("Invalid Email Address!");
+        }
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -11,6 +33,9 @@ class Contact {
     }
 }
 
-
+try{
 let contact1 = new Contact("Abhishek", "Sen", "Anand Nagar", "Bhopal", "Madhya Pradesh", "462022", "8083134796", "abhisheksen7023@gmail.com");
 console.log("Contact Created:", contact1);
+}catch(error){
+    console.error(error.message);
+}
